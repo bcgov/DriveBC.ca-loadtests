002c162d6a9c95a74e0bba309c85e4b8a29054d8
Mike Olund <mike@oxd.com>
2022-11-28 23:24:02 -0800

    DBCC-2: Add code analysis and linting pre-commit checks

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -1,6 +1,6 @@
 import os
 
-from .base import *
+from .base import *  # noqa
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
@@ -8,16 +8,20 @@ ALLOWED_HOSTS = [os.environ.get("PRODUCTION_HOST")]
 
 
 # White Noise configuration - http://whitenoise.evans.io/en/stable/django.html
-INSTALLED_APPS.extend(["whitenoise.runserver_nostatic"])
+INSTALLED_APPS.extend(["whitenoise.runserver_nostatic"])  # noqa F405
 
 # Must insert after SecurityMiddleware, which is first in settings/common.py
-MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")  # noqa F405
 
-TEMPLATES[0]["DIRS"] = [os.path.join(BASE_DIR, "../", "frontend", "build")]
+TEMPLATES[0]["DIRS"] = [os.path.join(BASE_DIR, "../", "frontend", "build")]  # noqa F405
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "../", "frontend", "build", "static")]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "../", "frontend", "build", "static")  # noqa F405
+]  # noqa F405
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # noqa F405
 
 STATIC_URL = "/static/"
-WHITENOISE_ROOT = os.path.join(BASE_DIR, "../", "frontend", "build", "root")
+WHITENOISE_ROOT = os.path.join(
+    BASE_DIR, "../", "frontend", "build", "root"  # noqa F405
+)  # noqa F405
---
+++ backend/contrib/serializers.py
@@ -0,0 +1,3 @@
+from rest_framework import serializers  # noqa F401
+
+# Create your serializers here.
