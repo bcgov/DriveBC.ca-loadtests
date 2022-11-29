7b2ef2b97f42808a49e212c9d2b3bbbaece399ef
Mike Olund <mike@oxd.com>
2022-11-28 21:29:14 -0800

    Update react to latest version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,5 @@
-import React from 'react';
 import logo from './logo.svg';
-import axios from 'axios';
 import './App.css';
-
-require('dotenv').config()
-
-function handleSubmit(event) {
-  const text = document.querySelector('#char-input').value
-
-  axios
-    .get(`/char_count?text=${text}`).then(({data}) => {
-      document.querySelector('#char-count').textContent = `${data.count} characters!`
-    })
-    .catch(err => console.log(err))
-}
 
 function App() {
   return (
@@ -23,14 +9,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <div>
-        <label htmlFor='char-input'>How many characters does </label>
-        <input id='char-input' type='text' placeholder="my string"/><span> </span>
-        <button onClick={handleSubmit}>have?</button>
-        <div>
-          <h3 id='char-count' data-testid="char-count"> </h3>
-        </div>
-      </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
