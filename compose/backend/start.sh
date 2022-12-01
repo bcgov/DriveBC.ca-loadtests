e08a92939b2bbff69f000276cfd6db3e5e43c13c
Mike Olund <mike@oxd.com>
2022-11-30 20:58:42 -0800

    DBCC-00: Ignore existent admin user error on start

--- compose/backend/start.sh
+++ compose/backend/start.sh
@@ -9,6 +9,6 @@ python manage.py migrate
 python manage.py createsuperuser \
     --noinput \
     --username $DJANGO_SUPERUSER_USERNAME \
-    --email $DJANGO_SUPERUSER_EMAIL
+    --email $DJANGO_SUPERUSER_EMAIL || true
 
 python manage.py runserver 0.0.0.0:8000
