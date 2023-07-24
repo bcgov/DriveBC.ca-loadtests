76d7ba7382f2cba861e8394e7526125c7695e96e
Jason Stewart <jason@oxd.com>
2023-07-24 16:29:22 -0700

    Remove dead code and format frontend.py

--- locustfiles/frontend.py
+++ locustfiles/frontend.py
@@ -1,12 +1,5 @@
-# import os
 from locust import HttpUser, task, events, between
 
-# test_env = os.environ.get("TEST_ENV")
-
-@events.init_command_line_parser.add_listener
-def _(parser):
-    # Choices will validate command line input and show a dropdown in the web UI
-    parser.add_argument("--env", choices=["dev", "test", "prod"], default="test", help="Environment")
 
 class DriveBcUsers(HttpUser):
     wait_time = between(1, 5)
@@ -17,8 +10,9 @@ class DriveBcUsers(HttpUser):
         self.client.get("/static/js/main.2f8c977e.js")
         self.client.get("/static/css/main.77abc6c4.css")
         self.client.get("/static/css/main.77abc6c4.css")
-        self.client.get("/static/media/BCSans-Regular.0079ea8e42d4e81a13d2.woff")
-        self.client.get("/static/media/BCID_H_rgb_pos.d9a69baa85bde64a610a.png")
+        self.client.get(
+            "/static/media/BCSans-Regular.0079ea8e42d4e81a13d2.woff")
+        self.client.get(
+            "/static/media/BCID_H_rgb_pos.d9a69baa85bde64a610a.png")
         self.client.get("/manifest.json")
         self.client.get("/favicon.ico")
-
