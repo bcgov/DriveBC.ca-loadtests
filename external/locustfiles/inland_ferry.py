0294d12d1278fbc940319a12f8f036e5e040c990
Jason Stewart <jason@oxd.com>
2024-02-20 16:56:49 -0800

    Make inland_ferry conform to other test format

--- external/locustfiles/inland_ferry.py
+++ external/locustfiles/inland_ferry.py
@@ -4,11 +4,15 @@ from locust import FastHttpUser, task, events
 class DriveBcUser(FastHttpUser):
     @task
     def backend(self):
-        with self.client.get("/api/cms/ferries/", catch_response=True) as response:
+        self.client.get("/api/cms/ferries/")
 
-            try:
-                assert response.status_code == 200
-            except AssertionError as err:
-                err = "Response type does not match"
-                response.failure(err)
-                return False, err
+        ## DEBUG calls. Turn these on to confirm path/asset validity.
+
+        # with self.client.get("/api/cms/ferries/", catch_response=True) as response:
+
+        #     try:
+        #         assert response.status_code == 200
+        #     except AssertionError as err:
+        #         err = "Response type does not match"
+        #         response.failure(err)
+        #         return False, err
