8ce744f54ae1c6c8c48db07b2e2fa37ad7c654a5
Mulder <Willem.Mulder@gov.bc.ca>
2024-03-06 13:17:43 -0800

    Update quick_static_test.py

--- locustfiles/quick_static_test.py
+++ locustfiles/quick_static_test.py
@@ -5,12 +5,12 @@ class staticUser(FastHttpUser):
 
     @task()
     def static(self):
-        self.client.get("/static/js/main.c462f54a.js")
+#        self.client.get("/static/js/main.c462f54a.js")
 #        self.client.get("/static/css/main.e088fd2e.css")
 #        self.client.get("/favicon.ico")
 #        self.client.get("/static/media/BCSans-Bold.252ecf87ea37c93b293c.woff")
 #        self.client.get("/static/media/BCSans-Regular.0079ea8e42d4e81a13d2.woff")
-#        self.client.get("/static/media/dbc-logo.db01b0c208d3fc8c9685639093aa724d.svg")
+        self.client.get("/static/media/dbc-logo.db01b0c208d3fc8c9685639093aa724d.svg")
 #        self.client.get("/static/media/BCHwyCrest1.3250c217efd2de2b79cf66242614ec2b.svg")
 #        self.client.get("/static/media/BCHwyCrest.9291029e030864484ab6e32a1356dc1a.svg")
 #        self.client.get("/static/media/colocated-camera.db3f3531300ceed4b5c080dc1ae007ff.svg")
