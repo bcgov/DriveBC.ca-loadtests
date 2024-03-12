beb27a2eb60a589bf4d4c3d7b300ceb089e3dd2c
Mulder <Willem.Mulder@gov.bc.ca>
2024-03-12 13:18:20 -0700

    Update quick_static_test.py

--- locustfiles/quick_static_test.py
+++ locustfiles/quick_static_test.py
@@ -5,11 +5,11 @@ class staticUser(FastHttpUser):
 
     @task()
     def static(self):
-        self.client.get("/static/js/main.c462f54a.js")
-#        self.client.get("/static/css/main.e088fd2e.css")
+        self.client.get("/static/js/main.8458c03c.js")
+        self.client.get("/static/css/main.88cf6bfe.css")
 #        self.client.get("/favicon.ico")
-        self.client.get("/static/media/BCSans-Bold.252ecf87ea37c93b293c.woff")
-        self.client.get("/static/media/BCSans-Regular.0079ea8e42d4e81a13d2.woff")
+#        self.client.get("/static/media/BCSans-Bold.252ecf87ea37c93b293c.woff")
+#        self.client.get("/static/media/BCSans-Regular.0079ea8e42d4e81a13d2.woff")
 #        self.client.get("/static/media/dbc-logo.db01b0c208d3fc8c9685639093aa724d.svg")
 #        self.client.get("/static/media/BCHwyCrest1.3250c217efd2de2b79cf66242614ec2b.svg")
 #        self.client.get("/static/media/BCHwyCrest.9291029e030864484ab6e32a1356dc1a.svg")
